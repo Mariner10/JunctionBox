Mariner10/JunctionBox

commit bdf92afda90663084550e9980a1bf3fee6177d2a

Fix map drawing

main.py
@@ -793,11 +793,10 @@ async def sendFile(linkName,request: apiRequest):
 async def mapCreation(mapModelName,redownload,current_user: Annotated[User, Depends(get_current_active_user)],request: apiRequest):
     logRequest(request)
 
-    
-    username = current_user.model_dump()['username']
-    deviceName = users_db[username]["device_name"]
     if bool(int(redownload)):
-        map_toolkit.theSauce(deviceName)
+        username = current_user.model_dump()['username']
+        deviceName = users_db[username]["device_name"]
+        map_toolkit.theSauce(device_name=deviceName)
     else:
         pass
     try:

commit d5a01a2b7be83d6e84ce075ba5e0015c226f221a

Fixing error introduced in commit bdf92af

main.py
@@ -793,9 +793,10 @@ async def sendFile(linkName,request: apiRequest):
 async def mapCreation(mapModelName,redownload,current_user: Annotated[User, Depends(get_current_active_user)],request: apiRequest):
     logRequest(request)
 
+    username = current_user.model_dump()['username']
+    deviceName = users_db[username]["device_name"]
+
     if bool(int(redownload)):
-        username = current_user.model_dump()['username']
-        deviceName = users_db[username]["device_name"]
         mpToolkit = map_toolkit(deviceName)
         mpToolkit.theSauce()
     else: